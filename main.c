3f0fed77da343fd0a3b3a0352447a5aad8aa6dbb
jun7 <jun7@hush.com>
2018-01-26 14:08:31 +0900

    Fix #10 what wrong modifier check

--- main.c
+++ main.c
@@ -1991,8 +1991,10 @@ static gchar *ke2name(GdkEventKey *ke)
 	}
 	g_strfreev(swaps);
 
-	gint len = sizeof(dkeys) / sizeof(*dkeys);
-	guint mask = ((ke->state & GDK_MODIFIER_MASK) & ~GDK_SHIFT_MASK);
+	guint mask = ke->state & (~GDK_SHIFT_MASK &
+			gdk_keymap_get_modifier_mask(gdk_keymap_get_default(),
+				GDK_MODIFIER_INTENT_DEFAULT_MOD_MASK));
+	static gint len = sizeof(dkeys) / sizeof(*dkeys);
 	for (int i = 0; i < len; i++) {
 		Keybind b = dkeys[i];
 		if (key == b.key && b.mask == mask)
