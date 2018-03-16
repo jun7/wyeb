4990dec335610c1cc90d3445954a7235fe9dde87
jun7 <jun7@hush.com>
2018-03-17 01:04:43 +0900

    Update comment

--- general.c
+++ general.c
@@ -148,7 +148,7 @@ Conf dconf[] = {
 	{"set:image" , "linkformat"   , "[![](%s) %.40s](%s)"},
 	{"set:image" , "linkdata"     , "ftu"},
 	{"set:image" , "hintstyle"    ,
-		"font-size:small !important;-webkit-transform:rotate(-9deg)"},
+		"font-size:medium !important;-webkit-transform:rotate(-9deg)"},
 
 	{DSET    , "search"           , "https://www.google.com/search?q=%s", "search=g"},
 	{DSET    , "usercss"          , "user.css;u2.css;u3.css"},
