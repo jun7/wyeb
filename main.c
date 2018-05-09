a269dbce734569867cb6f5703ffcceff83c2d8e1
jun7 <jun7@hush.com>
2018-05-09 19:20:42 +0900

    typo

--- main.c
+++ main.c
@@ -2140,7 +2140,7 @@ static Keybind dkeys[]= {
 	{"findclipboard" , 0, 0},
 	{"findsecondary" , 0, 0},
 
-	{"tohintopen"    , 0, 0, "not click but open uri as opennew/back"},
+	{"tohintopen"    , 0, 0, "not click but opens uri as opennew/back"},
 
 	{"openback"      , 0, 0},
 	{"openwithref"   , 0, 0, "Current uri is sent as Referer"},
